86f800a9b30d08c3349052172275aa3d1c91882c
samarth-thaker <samarth.thaker07@gmail.com>
2024-07-24 22:42:22 +0530

    Planning page layout

--- lib/plan.dart
+++ lib/plan.dart
@@ -22,10 +22,48 @@ class Planning extends StatelessWidget {
   void more(BuildContext context) {
     Navigator.pushNamed(context, '/more');
   }
-
+  void plannedpayments(){}
+  void budgets(){}
+  Widget customTile(String title, VoidCallback onTap) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: ListTile(
+        contentPadding:
+          const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+      title: Text(title,
+          style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black)),
+      trailing: const Icon(Icons.add_circle_sharp, color: Colors.black, size: 30,),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18),
+      ),
+      tileColor:const Color.fromARGB(249, 128, 128, 128),
+      onTap: onTap,
+      ),
+      
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Planning',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+        ),
+      ),
+      body: ListView(children: [
+       const SizedBox(height: 18),
+       customTile("Planned Payments", plannedpayments),
+            const SizedBox( height: 18,),
+            customTile("Budgets", budgets),
+            const SizedBox( height: 18,),
+      ],
+      ), 
       bottomNavigationBar: ValueListenableBuilder<int>(
           valueListenable: _selectedIndex,
           builder: (context, selectedIndex, child) {
@@ -79,6 +117,7 @@ class Planning extends StatelessWidget {
               },
             );
           }),
+      
     );
   }
 }
